f9991a29547e4ddcb46c219874f9a88c487a94ab
fojuth <fojuth@gmail.com>
2013-09-03 16:26:13 +0200

    Upload handler can be sent via $_GET (but then it must be defined in the config).

--- src/Fojuth/Plupload/controllers/PluploadController.php
+++ src/Fojuth/Plupload/controllers/PluploadController.php
@@ -15,16 +15,23 @@ class PluploadController extends \Illuminate\Routing\Controllers\Controller {
    * @throws \LogicException
    */
   public function gate() {
-    $class_name = \Config::get('plupload::plupload.upload_handler');
-    $this->upload_handler = new $class_name;
     
+    // The upload handler class name can be set as a GET parameter, but it must exist in the config file
+    if (true === isset($_GET['upload_handler'])) {
+      $handler = (string) str_replace('::', '\\', $_GET['upload_handler']);
+      
+      $class_name = \Config::get('plupload::plupload.upload_handler_'.$handler);
+    }
+    else {
+      $class_name = \Config::get('plupload::plupload.upload_handler');
+    }
+
+    $this->upload_handler = new $class_name;
+
     // The upload handler must implement a specific interface
     if (false === $this->upload_handler instanceof \Fojuth\Plupload\UploadHandlerInterface) {
       throw new \LogicException('The upload handler must implement the \Fojuth\Plupload\UploadHandlerInterface interface.');
     }
-    
-    // We register the GET params
-    $this->upload_handler->registerParams($_GET);
     
     // All looks fine, we start the upload
     return $this->upload_handler->upload(\Input::file('file'));
